6f772b2ca9fac2c4e0b477152b96c2270de38e59
don-quixotee <Abhishek>
2020-07-13 03:46:42 +0530

    implemented review

--- yolo/settings.py
+++ yolo/settings.py
@@ -63,6 +63,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 
